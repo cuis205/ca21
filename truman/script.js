6b8f7ddfeeff9693c686fa8b2878ca54e23f6fc2
Stephanie Cui <stephaniecui@Stephanies-MacBook-Pro.local>
2021-03-29 18:49:44 -0400

    a felony

--- truman/script.js
+++ truman/script.js
@@ -8,19 +8,27 @@
   //array2 = buildPage(DATA);
    //});
 
+
+
 function buildPage(data){
 smith = DATA.sort(function (a, b) {
   return a.Year - b.Year;
 });
+
 
   var row;
   // for (var i=0; i<films.length; i++){
   //function scope
  Array.from(smith).forEach((objects,i) => {
 
-    if (i<10){
+    if (i<1){
       row = document.querySelector('.items');
     }
+
+    $( ".items" ).append('<div id="id' +
+            (i+1) +
+            '" class="inbetweener"> </div>');
+
 
 
      //makes an element
@@ -30,15 +38,16 @@ smith = DATA.sort(function (a, b) {
 
     //images
     var img = document.createElement('img');
-    img.src = tiles.Bit;   // The image source from JSON array.
+    //img.src = "http://localhost:8000/" + tiles.Bit;   // The image source from JSON array.
     el.appendChild(img);
-    img.setAttribute("id","spin");
+    img.setAttribute("class","spin");
+
 
     //endimage
 
     // el.innerHTML=tiles.Film;
     el.setAttribute("class","sizing");
-    el.setAttribute("id",`"${tiles.Year}"`);
+    img.setAttribute("id",`"${tiles.Er}"`);
     //adds the element to the row
     row.appendChild(el);
     console.log(objects.Year);
@@ -49,8 +58,36 @@ smith = DATA.sort(function (a, b) {
 
 };
 
-//ending
 
+//ending
+function spin(){
+  let spin = document.querySelectorAll(".spin");
+  spin.forEach((item, i) => {
+    let rotation=4+Math.random()*1;
+    rotation=rotation*(Math.random()>0.5?1:-1);
+    item.style.setProperty('--rotate-amount', rotation+'deg');
+  });
+};
+
+function inbt(){
+  let inbtw = document.querySelectorAll(".inbetweener");
+  inbtw.forEach((item, i) => {
+    let space= 25*(i+1);
+    item.style.width = space + 'px';
+  });
+};
+
+//sounds
+function Play(){
+    var audio = new Audio("wood_click.ogg");
+    audio.play();
+    audio.volume = 0.3;
+}
+//bkg
+function amb() {
+    var audio2 = new Audio("Sweden.mp3");
+       audio2.play();
+     }
 //popUp
 function popUp(objects){
   var stream = document.querySelector('.items');
@@ -64,10 +101,11 @@ stream.onclick = function (e) {
 
 
   $('#infopop').position({
-
    my: "left+3 top-1",
     of: e,
     collision: "flipfit" });
+
+    Play();
 };
 
 $('.objects').mouseleave(function() {
@@ -108,32 +146,75 @@ $('#infopop').fadeIn(50);
 
 
 
+
 window.onload = function(){
 var btn = document.getElementById('close-button');
 
  btn.onclick = function(event) {
   $('#infopop').css("display","none");
 };
-};
-
-window.onload = function(){
-  var spin =  document.querySelector('#spin');
-  var num = ['40','23','-3','9','-9'];
-  var rnum = Math.floor(Math.random()*num.length);
-  $('#spin:last').css({
-    "-webkit-transform": 'rotate(' + (Math.floor(Math.random()*10)-num[rnum]) + 'deg)',
-    "-moz-transform": 'rotate(' + (Math.floor(Math.random()*10)-num[rnum]) + 'deg)',
-  });
 
 };
 
-$(function() {
-  $(".ending").typed({
-    strings: ["People never touched one another. The custom had become obsolete, owing to the Machine."],
 
-    typeSpeed: 0,
-    showCursor:false,
-    smartBackspace:false,
-  });
+function callback(data){
+ r_temp = data.main.temp;
+ r_far = Math.round((r_temp - 32) * 5 / 9);
+ var weat = document.querySelector(".weather");
+ r_cloud = data.clouds.all;
+    console.log(r_cloud);
+    console.log(data);
+
+  if(r_cloud < 50){
+
+    	weat.innerHTML = '☁' + " " + r_temp + '°F' + "|" + r_far + '°C' ;
+  }else{
+        	weat.innerHTML = '' + " " + r_temp + '°F' + "|" + r_far + '°C' ;
+  };
+
+};
+
+$.ajax({
+    url:"http://api.openweathermap.org/data/2.5/weather?id=5134086&units=imperial&appid=1e830da7191978393386d3d7e98d3d89",
+    type: "GET",
+    dataType: "jsonp",
+        jsonp : "callback",
+        jsonpCallback: "callback"
+        });
+
+
+
+function timmy() {
+  var blinky = '<span>:</span>';
+
+  var time = document.querySelector(".time");
+  var d = new Date();
+  var s = d.getSeconds();
+  var m = d.getMinutes();
+  var h = d.getHours();
+  console.log(d);
+  time.innerHTML =
+    ("0" + h).substr(-2) + blinky + ("0" + m).substr(-2);
+};
+
+setInterval(timmy, 1000);
+
+function scrall(){
+  let scrEl = document.getElementById("scr-el")
+  scrEl.addEventListener('scroll', event => {
+  let scrolled = (scrEl.scrollLeft / (scrEl.scrollWidth - scrEl.clientWidth) ) * 100
+  document.getElementById("myBar").style.width = scrolled + "%"
 });
+};
+
+
+//$(function() {
+  //$(".ending").typed({
+  //  strings: ["People never touched one another. The custom had become obsolete, owing to the Machine."],
+
+  //  typeSpeed: 0,
+  //  showCursor:false,
+  //  smartBackspace:false,
+//  });
+//});
 //};
