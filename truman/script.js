79da6c256df43c5922dad98177801a69c485c870
Stephanie Cui <stephaniecui@Stephanies-MacBook-Pro.local>
2021-03-29 21:14:42 -0400

    struggle

--- truman/script.js
+++ truman/script.js
@@ -99,7 +99,7 @@ stream.onclick = function (e) {
 	// 'left': e.pageX + 250,
 	// 'top': e.pageY +150,});
 
-   $( "#but" ).draggable();
+   $( "#infopop" ).draggable();
   $('#infopop').position({
    within: "window"
     //of: e,
